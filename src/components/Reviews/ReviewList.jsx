ce6951581b5f6ab1ac3692f18d82114666804972
tyebean <flower.webster@gmail.com>
2022-03-25 17:56:32 -0400

    test

---
+++ src/components/Reviews/ReviewList.jsx
@@ -0,0 +1,17 @@
+
+
+const ReviewList = () => {
+  return ( 
+    <div>
+      <h1> hey! the reviews of a specific game go here! </h1>
+      
+    </div>
+  );
+}
+
+export default ReviewList; 
+
+// gameId from the review model in the back end
+// if gameid(review mod) === apiId(game mod)
+// then map
+
