14ce363d302064f94580b6b874fd5edc01d4a677
tyebean <flower.webster@gmail.com>
2022-03-28 10:19:56 -0400

    add style and indentation

--- src/components/GameDetails/GameDetails.jsx
+++ src/components/GameDetails/GameDetails.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import { getGameDetails } from '../../services/gameServices';
 import ReviewForm from '../Reviews/ReviewForm/ReviewForm';
-import styles from './GameDetails.css'
+// import styles from './GameDetails.css'
 
 const GameDetails = () => {
   const [gameDetails, setGameDetails] = useState(null)
@@ -14,30 +14,29 @@ const GameDetails = () => {
     getGameDetails(location.state.gameDetails.id)
       .then(gameData => setGameDetails(gameData))
   }, [])
-// console.log(gameDetails)
+  // console.log(gameDetails)
 
   return (
     <>
-      <div className={styles.container}> 
+      <div>
         {gameDetails ?
-        <div className={styles.detailsContainer}>
-          <h3>{gameDetails.name}</h3>
-          <img 
-          src={gameDetails.background_image} 
-          alt={gameDetails.name}/>
-          {/* <h3>Details: {gameDetails.details}</h3> */}
-          <Link to='/game'>Back to Games</Link>
-        </div>
-      : 
-        <h2 className={styles.loading}>loading</h2>}
-    </div>
-
-    <div 
-    className='reviews-and-comments' >
-    <ReviewForm />
-    </div>
+          <div>
+            <h3>{gameDetails.name}</h3>
+            <img
+              src={gameDetails.background_image}
+              alt={gameDetails.name} />
+            {/* <h3>Details: {gameDetails.details}</h3> */}
+            <Link to='/game'>Back to Games</Link>
+          </div>
+          :
+          <h2 >loading</h2>}
+      </div>
+      <div
+        className='reviews-and-comments' >
+        <ReviewForm />
+      </div>
     </>
   );
 }
 
-export default GameDetails;
+export default GameDetails; 
