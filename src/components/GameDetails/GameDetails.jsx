12014cc2f56438879150ab21eb65c0610798f055
joewcode <jweathingtonsr@gmail.com>
2022-03-28 20:46:23 -0500

    transfer gamedetails state from details page to review component to use to capture ID

--- src/components/GameDetails/GameDetails.jsx
+++ src/components/GameDetails/GameDetails.jsx
@@ -1,49 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
-import { getGameDetails } from '../../services/gameServices';
-import ReviewForm from '../Reviews/ReviewForm/ReviewForm';
-import * as reviewService from '../../services/reviewService' 
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { getGameDetails } from "../../services/gameServices";
+import ReviewForm from "../Reviews/ReviewForm/ReviewForm";
+import * as reviewService from "../../services/reviewService";
 // import styles from './GameDetails.css'
 
 const GameDetails = (props) => {
-  const [reviews, setReviews] = useState([])
-  const handleAddReview = async newFormData => {
-    const newReview = await reviewService.create(newFormData)
-    setReviews([...reviews, newReview])
-  }
+  const [reviews, setReviews] = useState([]);
+  const handleAddReview = async (newFormData) => {
+    const newReview = await reviewService.create(newFormData);
+    setReviews([...reviews, newReview]);
+  };
 
-  const [gameDetails, setGameDetails] = useState(null)
-  let location = useLocation()
+  const [gameDetails, setGameDetails] = useState(null);
+  let location = useLocation();
   // console.log(location.state.gameDetails)
 
   useEffect(() => {
-    getGameDetails(location.state.gameDetails.id)
-      .then(gameData => setGameDetails(gameData))
-  }, [location.state.gameDetails.id])
-  // console.log(gameDetails)
+    getGameDetails(location.state.gameDetails.id).then((gameData) =>
+      setGameDetails(gameData)
+    );
+  }, [location.state.gameDetails.id]);
+  // console.log(gameDetails);
 
   return (
     <>
       <div>
-        {gameDetails ?
+        {gameDetails ? (
           <div>
             <h3>{gameDetails.name}</h3>
-            <img
-              src={gameDetails.background_image}
-              alt={gameDetails.name} />
+            <img src={gameDetails.background_image} alt={gameDetails.name} />
             {/* <h3>Details: {gameDetails.details}</h3> */}
-            <Link to='/game'>Back to Games</Link>
+            <Link to="/game">Back to Games</Link>
           </div>
-          :
-          <h2 >loading</h2>}
+        ) : (
+          <h2>loading</h2>
+        )}
       </div>
-      <div
-        className='reviews-and-comments' >
-        <ReviewForm handleAddReview={handleAddReview} />
+      <div className="reviews-and-comments">
+        {gameDetails && (
+          <ReviewForm
+            handleAddReview={handleAddReview}
+            // id={gameDetails.id}
+            // image={gameDetails.background_image}
+            gameDetails={gameDetails}
+          />
+        )}
       </div>
     </>
   );
-}
+};
 
-export default GameDetails; 
+export default GameDetails;
--- src/components/Reviews/ReviewForm/ReviewForm.jsx
+++ src/components/Reviews/ReviewForm/ReviewForm.jsx
@@ -3,52 +3,55 @@ import { useState } from "react";
 
 // todo: we want user to be logged in to do this. use above code.
 
-const ReviewForm = (props) => {
+const ReviewForm = props => {
   const [value, setValue] = useState({
     text: "",
-  })
-  const [submitted, setSubmitted] = useState(false)
-  const [valid, setValid] = useState(false)
+    image: props.gameDetails.background_image,
+    apiId: props.gameDetails.id,
+  });
+  const [submitted, setSubmitted] = useState(false);
+  const [valid, setValid] = useState(false);
 
-  const handleTxtChange = ( event ) => {
-    setValue({...value, text: event.target.value})
-  }
+  const handleTxtChange = event => {
+    setValue({ ...value, text: event.target.value });
+  };
 
-  const handleSubmit = ( event ) => {
+  const handleSubmit = event => {
     event.preventDefault();
-    props.handleAddReview(value)
+    props.handleAddReview(value);
     if (value.text) {
-      setValid(true)
+      setValid(true);
     }
-    setSubmitted(true)
-  }
+    setSubmitted(true);
+  };
+  console.log(props.gameDetails.id);
 
-  return ( 
+  return (
     <>
       <div className="form-container">
-        <form
-        className="review-form" 
-        onSubmit={handleSubmit}
-        >
-        {submitted && valid ?  <div className="success-msg">Review Submit Successful 🎉</div> : null }
-        {submitted && !value.text ? <span>Add a review first. It's not that hard.</span> : null }
+        <form className="review-form" onSubmit={handleSubmit}>
+          {submitted && valid ? (
+            <div className="success-msg">Review Submit Successful 🎉</div>
+          ) : null}
+          {submitted && !value.text ? (
+            <span>Add a review first. It's not that hard.</span>
+          ) : null}
 
-        <input 
-        onChange={handleTxtChange}
-        value={value.text}
-        type="text"
-        className="form-field"
-        placeholder="What are your thoughts?"
-        name="text"
-        />
-        <button 
-        className="review-form-btn" 
-        type="submit"
-        >Post</button> 
+          <input
+            onChange={handleTxtChange}
+            value={value.text}
+            type="text"
+            className="form-field"
+            placeholder="What are your thoughts?"
+            name="text"
+          />
+          <button className="review-form-btn" type="submit">
+            Post
+          </button>
         </form>
       </div>
     </>
   );
-}
+};
 
-export default ReviewForm; 
+export default ReviewForm;
