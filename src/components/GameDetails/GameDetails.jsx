03fc027e8cbe4e4843e8ce6597cfc48b0369b056
tyebean <flower.webster@gmail.com>
2022-03-29 17:29:21 -0400

    game details page, image and descriptio in flex row

--- src/components/GameDetails/GameDetails.jsx
+++ src/components/GameDetails/GameDetails.jsx
@@ -4,8 +4,8 @@ import { useLocation } from "react-router-dom";
 import { getGameDetails } from "../../services/gameServices";
 import ReviewForm from "../../components/Reviews/ReviewForm/ReviewForm";
 import * as reviewService from "../../services/reviewService";
+import styles from '../GameDetails/GameDetails.module.css'
 
-// import styles from './GameDetails.css'
 
 const GameDetails = props => {
   const [reviews, setReviews] = useState([]);
@@ -25,49 +25,63 @@ const GameDetails = props => {
     );
   }, [location.state.gameDetails.id]);
 
-  console.log(gameDetails);
 
   return (
-    <div className="icon-container">
+    <div className="all-content">
       {gameDetails ? (
-        <div className="game-card">
-          <h3>{gameDetails.name}</h3>
-          <img src={gameDetails.background_image} alt={gameDetails.name} />
-          <h3>Game Description: {gameDetails.description_raw}</h3>
-          <h3>Release Date: {gameDetails.released}</h3>
-          <h3>
-            Genres:{" "}
+        <div className={styles.detailsBody}> 
+          <h1>{gameDetails.name}</h1> 
+          
+            {/* <div className={styles.flex}> */}
+          <div className={styles.imgAndDesc}>
+          <img 
+          className={styles.detailsImg} 
+          src={gameDetails.background_image} 
+          alt={gameDetails.name} />
+          <h2 className={styles.desc}>Game Description</h2>
+          <p className={styles.desc}>{gameDetails.description_raw}</p>
+          </div>
+          <h3>Release Date</h3> 
+          <p>{gameDetails.released}</p> 
+          <h3>Genre</h3>
+          <p> {" "}
             {gameDetails.genres.map((genre, index) => (
               <div key={index} state={{ genre }}>
                 {genre.name}
               </div>
-            ))}
-          </h3>
-          <h3>
-            Developers:{" "}
+            ))}</p>
+            <h3>Developers</h3> 
+          <p>{" "}
             {gameDetails.developers.map((developer, index) => (
               <div key={index} state={{ developer }}>
                 {developer.name}
               </div>
             ))}
-          </h3>
-          <h3>
-            Platforms:{" "}
+          </p>
+          <h3>Platforms</h3>
+          <p>{" "}
             {gameDetails.platforms.map((platform, index) => (
               <div key={index} state={{ platform }}>
                 {platform.platform.name}
               </div>
             ))}
-          </h3>
-          <h3>Metacritic Rating: {gameDetails.metacritic}</h3>
+          </p>
+          <h3>Metacritic Rating</h3>
+          <p>{gameDetails.metacritic}</p>
+          {/* </div> */}
+
+          <div className="review-container">
+            <section className="reviews">
           <ReviewForm
             handleAddReview={handleAddReview}
             gameDetails={gameDetails}
           />
-          <Link to="/games">Return to Game Page</Link>
+            </section>
+          </div>
+          <Link to="/games">All Games</Link>
         </div>
       ) : (
-        <h2>loading</h2>
+        <p>loading</p>
       )}
     </div>
   );
