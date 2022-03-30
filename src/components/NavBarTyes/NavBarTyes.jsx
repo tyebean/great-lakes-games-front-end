dabbed575fed527bb632fe15a3cded913e20de96
tyebean <flower.webster@gmail.com>
2022-03-29 23:04:20 -0400

    edit navbar

--- src/components/NavBarTyes/NavBarTyes.jsx
+++ src/components/NavBarTyes/NavBarTyes.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from "react";
 
 const NavBarTyes = ({ user, handleLogout }) => {
+  const [expanded, setExpanded] = useState(false);
+
   return (
     <>
       {user ?
-        <nav className="navbar navbar-expand-lg navbnavbar-dark bg-dark">
-          <Link className="navbar-brand" to="/">
+        <nav className="navbar navbar-expand-lg bg-light">
+          <Link className="navbar-brand" to="/" onClick={() => setExpanded(expanded ? false : "expanded")}>
             <img 
             src="/assets/mystery_block.png" 
             width="30" 
@@ -15,36 +19,37 @@ const NavBarTyes = ({ user, handleLogout }) => {
             </img>
             Welcome, {user.name}
         </Link>
-        <button 
-        className="navbar-toggler" 
-        type="button" 
-        data-toggle="collapse" 
-        data-target="#navbarNav" 
-        aria-controls="navbarNav" 
-        aria-expanded="false" 
-        aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+          <button
+          onClick={() => setExpanded(expanded ? false : "expanded")}
+          className="navbar-toggler" 
+          type="button" 
+          data-toggle="collapse" 
+          data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
         <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <Link className="nav-link" to="/games">Games</Link>
+            <Link className="nav-link" to="/games" onClick={() => setExpanded(expanded ? false : "expanded")}>Games</Link>
           </li>
           <li className="nav-item active">
-            <Link className="nav-link" to="/profiles">Profiles</Link>
+            <Link className="nav-link" to="/profiles" onClick={() => setExpanded(expanded ? false : "expanded")}>Profiles</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/changePassword">Change Password</Link>
+            <Link className="nav-link" to="/changePassword" onClick={() => setExpanded(expanded ? false : "expanded")}>Change Password</Link>
           </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="" onClick={handleLogout}>LOG OUT</Link>
-          </li>
+          {/* <li className="nav-item">
+            <Link 
+            className="nav-link"
+            to="" onClick={handleLogout} >LOG OUT</Link>
+          </li> */}
         </ul>
         </div>
         </nav>
       :
         <nav className="navbar navbar-expand-lg navbnavbar-dark bg-dark">
-          <button 
+          <button
+          onClick={() => setExpanded(expanded ? false : "expanded")}
           className="navbar-toggler" 
           type="button" data-toggle="collapse"
           data-target="#navbarNav"
