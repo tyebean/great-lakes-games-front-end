60cb3b4de820cbd458117b7e58fe13bff98a81ff
joewcode <jweathingtonsr@gmail.com>
2022-03-30 13:45:29 -0500

    display reviews

--- src/components/Reviews/ReviewList/ReviewList.jsx
+++ src/components/Reviews/ReviewList/ReviewList.jsx
@@ -1,22 +1,17 @@
-
 const ReviewList = props => {
-  console.log("props.reviews are", props.reviews);
-  console.log("TEST", props.reviews.text)
-  return ( 
+  console.log(props.reviews);
+  return (
     <div>
-      <p> Hey! the reviews of a specific game go here! </p>
-      {/* {props.reviews.map (review => 
-        <p key={props.review._id}>
-          {props.review.text}
-          </p>
-        )}  */}
+      Hello am i working?
+      {props.reviews.map((review, index) => (
+        <div key={index}>{review.text}</div>
+      ))}
     </div>
   );
-}
+};
 
-export default ReviewList; 
+export default ReviewList;
 
 // gameId from the review model in the back end
 // if gameid(review mod) === apiId(game mod)
 // then map
-
