6e6d3607351f81388407c5c95f012894d0561927
tyebean <flower.webster@gmail.com>
2022-03-31 17:11:33 -0400

    style footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,67 +1,15 @@
 import styles from '../Footer/Footer.module.css'
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <>
       <footer className={styles.footer}>
-        {/* <div className={styles.bigFlex}> */}
-
-          <div className={styles.projectFlex}>
-            <p>The Project</p>
-            {/* <p>1</p>
-            <p>2</p> */}
-          </div>
-
-          <div className={styles.findUsFlex}>
-            <p>Find Us</p>
-            <p>Joseph</p>
-            <img 
-            src="/assets/github_logo.png" 
-            width="40" 
-            height="40"
-            className={styles.hubImg}
-            alt="github logo"></img>
-            <img 
-            src="/assets/linkedin_logo.png" 
-            width="40" 
-            height="40"
-            className={styles.linkImg}
-            alt="github logo"></img>
-            <p>Dan</p>
-            <img 
-            src="/assets/github_logo.png" 
-            width="40" 
-            height="40"
-            className={styles.hubImg}
-            alt="github logo"></img>
-            <img 
-            src="/assets/linkedin_logo.png" 
-            width="40" 
-            height="40"
-            className={styles.linkImg}
-            alt="github logo"></img>
-            <p>Tye</p>
-            <img 
-            src="/assets/github_logo.png" 
-            width="40" 
-            height="40"
-            className={styles.hubImg}
-            alt="github logo"></img>
-            <img 
-            src="/assets/linkedin_logo.png" 
-            width="40" 
-            height="40"
-            className={styles.linkImg}
-            alt="github logo"></img>
-          </div>
-
-
-          <div className={styles.apiFlex}>
-            <p>The API</p>
-            <p>RAWG.io</p>
-          </div>
-
-        {/* </div> */}
+        <p className={styles.api}>The API</p>
+        <Link
+        className={styles.rawg}
+        to="/https://rawg.io/"
+        >rawg.io</Link>
       </footer>
     </>
   );
