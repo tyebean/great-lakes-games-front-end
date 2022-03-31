99cb848122d0e3f69bd63fe8a7176fff94a92a93
tyebean <flower.webster@gmail.com>
2022-03-31 10:57:13 -0400

    edit navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,87 +1,40 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import {Navbar, Nav, NavItem, NavDropdown, DropdownButton, MenuItem, CollapsibleNav} from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar'
+import { Nav, Button, Form, FormControl, Container, NavDropdown } from 'react-bootstrap';
 
 const NavBar = ({ user, handleLogout }) => {
-  const [expanded, setExpanded] = useState(false);
-  // onClick={() => setExpanded(expanded ? false : "expanded")}
-  
+
   return (
     <>
       {user ?
-      <>
-      <Navbar bg="dark" variant="dark">
-        <container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-        </container>
-      </Navbar>
-      <br />
-      <Navbar bg="primary" variant="dark">
-        <container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-        </container>
-      </Navbar>
-    
-      <br />
-      <Navbar bg="light" variant="light">
-        <container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-        </container>
-      </Navbar>
-    </>
-        
-        :
-
         <>
-  <Navbar bg="dark" variant="dark">
-    <container>
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    <Nav className="me-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-    </container>
-  </Navbar>
-  <br />
-  <Navbar bg="primary" variant="dark">
-    <container>
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    <Nav className="me-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-    </container>
-  </Navbar>
+        <Navbar bg="dark" variant="dark">
+            <Container>
+              <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+              </Nav>
+            </Container>
+          </Navbar>
+        </>
+        :
+        <>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+              </Nav>
+            </Container>
+          </Navbar>
+          <br />
 
-  <br />
-  <Navbar bg="light" variant="light">
-    <container>
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    <Nav className="me-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-    </container>
-  </Navbar>
-</>
+        </>
 
       }
     </>
