5c3cac365a82fa32f55a69d493ec95f262f960a2
djrich87 <djrich87@gmail.com>
2022-03-31 19:14:05 -0600

    add favicon

--- src/components/Reviews/ReviewList/ReviewList.jsx
+++ src/components/Reviews/ReviewList/ReviewList.jsx
@@ -7,11 +7,6 @@ function ReviewList({ reviews, handleDeleteReview, handleEditReview }) {
   return (
     <div>
       {reviews.map((review, index) => (
-        
-        // console.log(review.text)
-        // <div>{review.text}
-        // {review.date}
-        // </div>
         <ReviewCard
         key={index}
         review={review}
@@ -28,25 +23,3 @@ function ReviewList({ reviews, handleDeleteReview, handleEditReview }) {
 
 
 export default ReviewList;
-
-// gameId from the review model in the back end
-// if gameid(review mod) === apiId(game mod)
-// then map
-
-// <div key={index} className={styles.reviewCard}>
-//           <div key={review._id}> {review.text} </div>
-
-
-//           <button
-//             onClick={() => handleDeleteReview(review._id)}>
-//             Delete</button>
-
-//           {/* <button 
-//           onClick={() => handleEditReview(reviews._id)}
-//           state={{review}}>
-//           Edit Review</button> */}
-//           <EditReview
-//             handleEditReview={handleEditReview}
-//             reviews={reviews}
-//           />
-//         </div>
