7b3bacf65a8616cc605c6e89adb6c04a5f753945
tyebean <flower.webster@gmail.com>
2023-02-16 15:15:03 -0500

    update footer link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,12 @@
 import styles from '../Footer/Footer.module.css'
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <>
       <footer className={styles.footer}>
         <p className={styles.api}>The API</p>
-        <Link
-        className={styles.rawg}
-        to="/https://rawg.io/"
-        >rawg.io</Link>
+        <a className={styles.rawg} rel="noreferrer" target="_blank" href="http://rawg.io/">rawg.io</a>
       </footer>
     </>
   );
